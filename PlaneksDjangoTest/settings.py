0a4f1e60ea3fa1e6488976b8ebaa84e687458a97
Павло Дровнін <pashadrovnin.camo@gmail.com>
2021-11-23 16:14:15 +0200

    fix redis

--- PlaneksDjangoTest/settings.py
+++ PlaneksDjangoTest/settings.py
@@ -16,7 +16,7 @@ import os
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ.get('REDIS_URL'),
+        "LOCATION": os.environ.get('REDIS_TLS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
             "CONNECTION_POOL_KWARGS": {
